d8f0fbc39dc5617bb5c3631c1e8576d42d0b7117
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2022-06-08 19:10:20 -0300

    Added options menu for user icon in sidenav, includes profile, saved items and log out; updated dropdown directive

--- src/app/directives/dropdown.directive.ts
+++ src/app/directives/dropdown.directive.ts
@@ -1,26 +1,40 @@
-import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import tippy from 'tippy.js';
 
-@Directive({
-  selector: '[dropdown]'
-})
+@Directive({ selector: '[dropdown]' })
 
 export class DropdownDirective implements AfterViewInit {
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) { }
 
   @Input() dropdown!: HTMLElement;
   @Input() hover: boolean = false;
+  @Input() offset: any = [0, 10];
+  @Input() place: any = 'top';
+  @Input() target: any = null;
 
   ngAfterViewInit(): void {
+    // Create Tippy instance.
     tippy(this.elementRef.nativeElement, {
       animation: 'fade',
       content: this.dropdown,
       delay: this.hover ? [800, 200] : 0,
       duration: [200, 200],
       interactive: true,
+      offset: this.offset,
+      placement: this.place,
       theme: 'dropdown',
       trigger: this.hover ? 'mouseenter focus' : 'click',
+      triggerTarget: this.target,
     });
+    // Attach listener to close dropdowns upon clicking inside.
+    if (!this.hover) {
+      this.renderer.listen(this.elementRef.nativeElement._tippy.popper, 'click', () => {
+        this.elementRef.nativeElement._tippy.hide();
+      });
+    }
   }
 }
