1ff1668013af77211952d04cfac9c959a8357e63
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2022-07-03 02:29:34 -0300

    ParseUrls: Updated regexes to have better detection and allow to use all at the same time without breaking anything

--- src/app/pipes/parse-urls.pipe.ts
+++ src/app/pipes/parse-urls.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ParseUrlsPipe implements PipeTransform {
 
   urls: any = /(\b(https?|http|ftp|ftps|Https|rtsp|Rtsp):\/\/[A-Z0-9+&@#\/%?=~_|!:,.;-]*[-A-Z0-9+&@#\/%=~_|])/gim;
-  hashtags: any = /(^|\s)(#[a-z\d][\w-.]*)/ig;
-  mentions: any = /(^|\s)(@[a-z\d][\w-.]*)/ig;
-  emails: any = /(\S+@\S+\.[a-z]+)/gim;
+  hashtags: any = /((?<!\w.)|(?<=\s))(#[a-z\d][\w-.]*)(?<!\.)/ig;
+  mentions: any = /((?<!\w.)|(?<=\s))(@[a-z\d][\w-.]*)(?<!\.)/ig;
+  emails: any = /([\w-.]{2,}@\S+\.[a-z]+)/gim;
 
   private parseUrl(text: string) {
     if (text.match(this.urls)) {
@@ -18,18 +18,15 @@ export class ParseUrlsPipe implements PipeTransform {
       });
     }
     if (text.match(this.hashtags)) {
-      text = text.replace(this.hashtags, "<a href=\"/$2\" class=\"hashtag-link\">$1$2</a>");
+      text = text.replace(this.hashtags, "<a href=\"/$2\">$1$2</a>");
       text = text.replaceAll("href=\"/#", "href=\"/");
-      return text;
     }
     if (text.match(this.mentions)) {
-      text = text.replace(this.mentions, "<a href=\"/$2\" class=\"handle-link\">$1$2</a>");
+      text = text.replace(this.mentions, "<a href=\"/$2\">$1$2</a>");
       text = text.replaceAll("href=\"/@", "href=\"/");
-      return text;
     }
     if (text.match(this.emails)) {
       text = text.replace(this.emails, "<a href=\"mailto:$1\">$1</a>");
-      return text;
     }
     return text;
   }
